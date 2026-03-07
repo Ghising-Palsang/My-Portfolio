9e03dc461fc63cf8a06cf626ea2a165c4d9e3ce6
Ghising-Palsang <tmgpalsang789@gmail.com>
2026-03-07 14:34:42 +0545

    ui changes

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -17,31 +17,41 @@ const Skills = () => {
     { name: "Others", variant: "others", id:4 },
   ];
   return (
-    <section className="min-h-screen flex flex-col justify-center px-6 md:px-12 gap-9 relative border-b border-gray-700 md:gap-16">
-      {/* title */}
+    <section
+      className={`min-h-screen flex flex-col justify-center px-6 md:px-12 gap-9 relative border-b border-gray-700 md:gap-16 py-12 md:py-20`}
+    >
+      {/* Title */}
       <div className="max-w-3xl">
         <h1
-          className={`text-5xl tracking-wider font-extrabold ${isLightMode ? "text-gray-900" : "text-gray-100"} md:text-6xl lg:text-7xl lg:tracking-widest`}
+          className={`text-5xl tracking-wider font-extrabold ${
+            isLightMode ? "text-gray-900" : "text-gray-100"
+          } md:text-6xl lg:text-7xl lg:tracking-widest`}
         >
           Skills
         </h1>
       </div>
+
       <div className="space-y-5 md:space-y-10 lg:items-center lg:flex lg:flex-col lg:gap-4">
-        {/* text */}
+        {/* Text */}
         <p
-          className={`text-lg tracking-tight font-medium leading-snug ${isLightMode ? "text-gray-900" : "text-gray-400"} md:text-xl  lg:text-xl md:self-start`}
+          className={`text-lg tracking-tight font-medium leading-snug ${
+            isLightMode ? "text-gray-900" : "text-gray-400"
+          } md:text-xl lg:text-xl md:self-start`}
         >
           Tools and tech I love working with everyday.
         </p>
-        {/* logos */}
-        <div className="space-y-5 md:grid md:grid-cols-2 md:gap-5 md:mx-auto md:items-center lg:gap-7 lg:gap-x-48 ">
+
+        {/* Logos */}
+        <div className="space-y-5 md:grid md:grid-cols-2 md:gap-5 md:mx-auto md:items-center lg:gap-7 lg:gap-x-48">
           {categories.map(({ name, variant, id }) => (
             <div
-              className=" space-y-2 md:space-y-4 lg:space-y-6 min-h-40"
+              className="space-y-2 md:space-y-4 lg:space-y-6 min-h-40"
               key={id}
             >
               <h3
-                className={`text-lg tracking-wide font-semibold ${isLightMode ? "text-gray-900" : "text-gray-200"} md:text-xl `}
+                className={`text-lg tracking-wide font-semibold ${
+                  isLightMode ? "text-gray-900" : "text-gray-200"
+                } md:text-xl`}
               >
                 {name}
               </h3>
@@ -50,7 +60,9 @@ const Skills = () => {
           ))}
         </div>
       </div>
-      <div className="flex flex-wrap gap-4 mt-6 md:justify-center lg:justify-center  lg:gap-6">
+
+      {/* Buttons */}
+      <div className="flex flex-wrap gap-4 mt-6 md:justify-center lg:justify-center lg:gap-6">
         <Button href="Project">View Projects</Button>
         <Button variant="secondary" download href="../../public/resume.pdf">
           Resume
